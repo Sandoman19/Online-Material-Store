87a522cdb2e241cb2a24766926cd266725a4809e
Siddy <dancore99@outlook.com>
2021-11-23 21:32:55 +0800

    connection file complete - route skel working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,11 @@
 // importing express.router
-
 // import user routes
 // import product routes
+const router = require("express").Router();
+const productRoutes = require("./productRoute");
+const userRoutes = require("./userRoutes");
+
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,12 @@
 // importing express.router
-
+const router = require("express").Router();
 // importing models
+const { User } = require("../../models");
 
 // post for login see 23-AUTH review
-
+router.post("/login", async (req, res) => {});
 // post for log out see 23-AUTH review
+router.post("/logout", async (req, res) => {});
 
 // export
+module.exports = router;
