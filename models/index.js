2f757619670f4671bccedadc45c8caa1e548d41c
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-11-25 16:45:14 +0800

    model and seeds first push

--- models/index.js
+++ models/index.js
@@ -1,5 +1,22 @@
-// import model file
+const User = require('./User');
+const Product = require('./Product');
+const Brand = require('./Brand');
+const Material = require('./Material');
 
-// any foreignkeys that are needed
+Product.hasMany(Brand, {
+  foreignKey: 'product_id',
+});
 
-// export
+Brand.hasMany(Material, {
+  foreignKey: 'product_id',
+});
+
+Brand.belongsTo(Product, {
+  foreignKey: 'brand_id',
+});
+
+Material.belongsTo(Brand, {
+  foreignKey: 'brand_id',
+});
+
+module.exports = { User, Product, Material, Brand };
