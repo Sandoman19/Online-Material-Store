9d426872e696d76e6e5bb91202eee126e4942b5d
Siddy <dancore99@outlook.com>
2021-11-25 21:15:53 +0800

    server complete- creating a user has been added to the routing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,20 @@ const router = require("express").Router();
 // importing models
 const { User } = require("../../models");
 
+//Create a new user
+router.post("/new_user", async (req, res) => {
+  try {
+    const newUser = req.body;
+    // hash the password from 'req.body' and save to newUser
+    newUser.password = await bcrypt.hash(req.body.password, 10);
+    // create the newUser with the hashed password and save to DB
+    const userData = await User.create(newUser);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // post for login see 23-AUTH review
 router.post("/login", async (req, res) => {
   try {
