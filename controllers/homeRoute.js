0a734aee4ab0d96bda4d361443376839b96ce13d
Siddy <dancore99@outlook.com>
2021-12-02 21:38:16 +0800

    routes edited user login work - needs material routes working

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -2,16 +2,34 @@
 const router = require("express").Router();
 
 // importing models
-const { User } = require("../models");
+const { User, Product } = require("../models");
 const withAuth = require("../units/auth");
 
 // GET route with AUTH and render of homepage
 router.get("/", async (req, res) => {
   try {
+    // Get all posts and JOIN with user data
+    const productData = await Product.findAll();
+
+    // Serialize data so the template can read it
+    const products = productData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      products,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
     const userData = await User.findAll({
       atrributes: { exlude: ["password"] },
     });
-    const user = userData.forEach((product) => {
+    const users = userData.forEach((product) => {
       product.get({ plain: true });
     });
     res.render("homepage", {
@@ -25,7 +43,7 @@ router.get("/", async (req, res) => {
 // GET route to redirect to login
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/homepage");
+    res.redirect("/");
     return;
   }
   res.render("login");
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,18 +1,18 @@
 // add logout event
 const logout = async () => {
   // make POST request to end session
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
     // if logged out go to login page
-    document.location.replace('/');
+    document.location.replace("/");
   } else {
-    alert('Your are not logged out.');
+    alert("Your are not logged out.");
   }
 };
 
 // add listener for linking logout to view pages
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector("#logout").addEventListener("click", logout);
